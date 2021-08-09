cf41e3ddef78692e07ad7c1015afdf9aac3ef9f9
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2021-08-09 17:13:52 +0200

    occamy: Move SPL to SRAM
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- include/configs/pulp-platform_occamy.h
+++ include/configs/pulp-platform_occamy.h
@@ -4,15 +4,17 @@
 #include <linux/sizes.h>
 
 #ifdef CONFIG_SPL
-#define CONFIG_SPL_MAX_SIZE		0x00100000
-#define CONFIG_SPL_BSS_START_ADDR	0x85000000
-#define CONFIG_SPL_BSS_MAX_SIZE		0x00100000
-#define CONFIG_SYS_SPL_MALLOC_START	(CONFIG_SPL_BSS_START_ADDR + \
-					 CONFIG_SPL_BSS_MAX_SIZE)
+#define CONFIG_SPL_MAX_SIZE		0x00020000
+
+#define CONFIG_SPL_BSS_START_ADDR	0x70018000
+#define CONFIG_SPL_BSS_MAX_SIZE		0x00008000
+
+// We need more space than the SPM can provide, so put the heap into HBM.
+// malloc should only be available _after_ the HBM is initialised.
+#define CONFIG_SYS_SPL_MALLOC_START	0x84000000
 #define CONFIG_SYS_SPL_MALLOC_SIZE	0x00100000
 
-#define CONFIG_SPL_STACK	(0x80000000 + 0x001D0000 - \
-				 GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SPL_STACK	0x70018000
 #endif
 
 #define CONFIG_SYS_SDRAM_BASE		0x80000000
