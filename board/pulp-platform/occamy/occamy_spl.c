bbd40af495c91f3c49d11e0facde55968edf115c
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2021-08-18 09:52:10 +0200

    occamy: Disable AXI isolation of quadrants in SPL
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- board/pulp-platform/occamy/occamy_spl.c
+++ board/pulp-platform/occamy/occamy_spl.c
@@ -4,9 +4,20 @@
 #include <log.h>
 #include <asm/spl.h>
 
+#define SOC_CTRL_BASE 0x2000000
+#define ISOLATE_0 0xa0
+#define ISOLATE_1 0xa4
+#define ISOLATED_0 0xa8
+#define ISOLATED_1 0xac
+#define SOC_CTRL(reg) *(int32_t*)(SOC_CTRL_BASE + reg)
+
 
 int spl_board_init_f(void)
 {
+	/* disable AXI isolation */
+	SOC_CTRL(ISOLATE_0) = 0;
+	SOC_CTRL(ISOLATE_1) = 0;
+
 	return 0;
 }
 
